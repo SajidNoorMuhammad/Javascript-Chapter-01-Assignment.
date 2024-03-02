85ab31fea26e8a93dae035676545ebfb854c0fb4
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-03-03 01:24:58 +0500

    Add Work.

---
+++ app.js
@@ -0,0 +1,28 @@
+      // Chapter # 01. 
+        // Question # 01.
+        // alert ("Welcome To The Website");
+
+        // Question # 02.
+        // alert ("Error! Please enter a valid password.");
+
+        // Question # 03
+        // alert ("Welcome to JS Land... \nHappy Coding!");
+
+        // Question # 04
+        // alert ("Welcome to JS Land...");
+
+        // Question # 04 Part B
+        alert ("Happy Coding! \nPrevent this page from creating additional dialogs.");
+
+        // Question # 05
+
+        alert ("Hello... I can run JS through my web browser's console");
+        console.log ("Hello... I can run JS through my web browser's console")
+
+        // Question # 06
+        // Add Javascript Alert In Existing File.
+        // https://sajidnoormuhammad.github.io/Responsive-Tere-Website/
+
+        // Question # 07
+            // alert("Welcome to JS Land... \nHappy Coding!");
+            
